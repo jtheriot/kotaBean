660fac3963bc32fe75437fcf8a9ca7ef95b205ba
Justin Theriot <jntheriot504@gmail.com>
2019-03-17 11:41:55 -0700

    completed code, needs refactoring to be reusable easy

--- cheetah_similarity.py
+++ cheetah_similarity.py
@@ -1,6 +1,8 @@
 import numpy
 import pandas
 
+from scipy import spatial
+
 df = pandas.read_csv('/home/justin/kotaBean/data/animal_features.csv')
 
 def create_column_list(df):
@@ -10,6 +12,27 @@ def create_column_list(df):
             continue
 
         elif col == 'Habitat':
+            for val in df[col].unique():
+                try:
+                    for h in val.split(','):
+                        colName = '.'.join([col, h])
+
+                        if colName in colList:
+                            continue
+
+                        else:
+                            colList.append(colName)
+
+                except:
+                    colName = '.'.join([str(col), str(val)])
+
+                    if colName in colList:
+                        continue
+
+                    else:
+                        crecolList.append(colName)
+
+        elif col == 'Location':
             for val in df[col].unique():
                 try:
                     for h in val.split(','):
@@ -42,5 +65,57 @@ def create_column_list(df):
 
     return colList
 
-def create_binary_matrix(df):
-    binaryMatrix = numpy.
+def create_binary_matrix(df, colList):
+    binaryMatrix = numpy.zeros(shape = (len(df), len(colList)))
+
+    for index, row in df.iterrows():
+        j = 0
+        for item in colList:
+            col = colList[j].split('.')[0]
+            val = colList[j].split('.')[1]
+
+            dfVal = df.iloc[index][col]
+
+            try:
+                for listVal in dfVal.split(','):
+                    if listVal == val:
+                        binaryMatrix[index, j] = int(1)
+
+                    else:
+                        continue
+
+            except:
+                if dfVal == val:
+                    binaryMatrix[index, j] = int(1)
+
+                else:
+                    continue
+
+            j += 1
+
+    return binaryMatrix
+
+
+def measure_similarity(binaryMatrix, df):
+    df['Similarity_Score'] = ''
+    cheetah = binaryMatrix[0]
+    for i in range(len(binaryMatrix)):
+        result = spatial.distance.cosine(cheetah, binaryMatrix[i])
+        print(result)
+        df.at[i, 'Similarity_Score'] = result
+
+
+    return df
+
+
+
+
+
+
+
+
+
+
+
+
+ m
